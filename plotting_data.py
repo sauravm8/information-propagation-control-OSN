235ece72c2230e78974f67c55268cec6f072d57b
sauravm8 <sauravmukherjee1995@gmail.com>
2018-03-26 03:00:32 +0530

    Added initial code for plotting data

---
+++ plotting_data.py
@@ -0,0 +1,13 @@
+import logging
+import config
+import logging
+import os
+import matplotlib.pyplot as plt
+
+
+def plotting_data(data_points):
+    plt.axis([])
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
